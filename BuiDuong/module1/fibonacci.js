8385597aaff0d4a74e43f19c9e7dccc9a443013b
duongbdh <duongbdhe182316@fpt.edu.vn>
2026-01-22 16:29:19 +0700

    feat(module1): add Fibonacci interface

---
+++ BuiDuong/module1/fibonacci.js
@@ -0,0 +1,11 @@
+/**
+ * Fibonacci interface
+ * @param {number} n
+ * @param {boolean} flag
+ * @returns {number}
+ */
+function fun(n, flag) {
+  throw new Error("Not implemented");
+}
+
+module.exports = { fun };
